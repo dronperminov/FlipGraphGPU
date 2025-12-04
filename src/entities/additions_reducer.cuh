559f0856ba097d47c89d0a3f22974fcbe1b4adde
dronperminov <perminov@ispras.ru>
2025-12-05 00:22:55 +0300

    improve performance

--- src/entities/additions_reducer.cuh
+++ src/entities/additions_reducer.cuh
@@ -32,9 +32,9 @@ class AdditionsReducer {
 
     __device__ __host__ void updateSubexpressions();
     __device__ __host__ void replaceSubexpression(const Pair &subexpression);
-    __device__ __host__ void replaceExpression(int index, int i, int j, int varIndex);
+    __device__ __host__ void replaceExpression(int *expression, int &size, int index1, int index2, int varIndex);
 
-    __device__ __host__ bool containsVariables(int exprIndex, int variable1, int variable2, int &index1, int &index2) const;
+    __device__ __host__ int binarySearch(const int *expression, int size, int value, int start) const;
     __device__ Pair selectSubexpression(int mode, curandState &state) const;
 
     void showVariable(int variable, bool first) const;
@@ -111,6 +111,7 @@ __device__ __host__ void AdditionsReducer<maxExpressionsCount, maxVariablesCount
     realVariables = reducer.realVariables;
     freshVariables = reducer.freshVariables;
     mode = reducer.mode;
+    scale = reducer.scale;
 
     for (int index = 0; index < expressionsCount; index++) {
         expressionSizes[index] = reducer.expressionSizes[index];
@@ -240,14 +241,24 @@ __device__ __host__ void AdditionsReducer<maxExpressionsCount, maxVariablesCount
 template <size_t maxExpressionsCount, size_t maxVariablesCount, size_t maxExpressionLength, size_t maxSubexpressionsCount>
 __device__ __host__ void AdditionsReducer<maxExpressionsCount, maxVariablesCount, maxExpressionLength, maxSubexpressionsCount>::replaceSubexpression(const Pair &subexpression) {
     int varIndex = realVariables + freshVariables + 1;
-    int i, j;
 
     for (int index = 0; index < expressionsCount; index++) {
-        if (containsVariables(index, subexpression.i, subexpression.j, i, j)) {
-            replaceExpression(index, i, j, varIndex);
+        if (expressionSizes[index] < 2)
+            continue;
+
+        int i = binarySearch(expressions[index], expressionSizes[index], subexpression.i, 0);
+        if (i == -1)
+            continue;
+
+        int j = binarySearch(expressions[index], expressionSizes[index], subexpression.j, i + 1);
+        if (j == -1)
+            continue;
+
+        if (expressions[index][i] == subexpression.i && expressions[index][j] == subexpression.j) {
+            replaceExpression(expressions[index], expressionSizes[index], i, j, varIndex);
         }
-        else if (containsVariables(index, -subexpression.i, -subexpression.j, i, j)) {
-            replaceExpression(index, i, j, -varIndex);
+        else if (expressions[index][i] == -subexpression.i && expressions[index][j] == -subexpression.j) {
+            replaceExpression(expressions[index], expressionSizes[index], i, j, -varIndex);
         }
     }
 
@@ -257,26 +268,27 @@ __device__ __host__ void AdditionsReducer<maxExpressionsCount, maxVariablesCount
 }
 
 template <size_t maxExpressionsCount, size_t maxVariablesCount, size_t maxExpressionLength, size_t maxSubexpressionsCount>
-__device__ __host__ bool AdditionsReducer<maxExpressionsCount, maxVariablesCount, maxExpressionLength, maxSubexpressionsCount>::containsVariables(int exprIndex, int variable1, int variable2, int &index1, int &index2) const {
-    index1 = -1;
-    index2 = -1;
+__device__ __host__ int AdditionsReducer<maxExpressionsCount, maxVariablesCount, maxExpressionLength, maxSubexpressionsCount>::binarySearch(const int *expression, int size, int value, int start) const {
+    int low = start;
+    int high = size - 1;
+    int valueAbs = abs(value);
 
-    for (int i = 0; i < expressionSizes[exprIndex]; i++) {
-        if (expressions[exprIndex][i] == variable1) {
-            index1 = i;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        int midAbs = abs(expression[mid]);
 
-            if (index2 != -1)
-                return true;
+        if (midAbs < valueAbs) {
+            low = mid + 1;
         }
-        else if (expressions[exprIndex][i] == variable2) {
-            index2 = i;
-
-            if (index1 != -1)
-                return true;
+        else if (midAbs > valueAbs) {
+            high = mid - 1;
+        }
+        else {
+            return mid;
         }
     }
 
-    return false;
+    return -1;
 }
 
 template <size_t maxExpressionsCount, size_t maxVariablesCount, size_t maxExpressionLength, size_t maxSubexpressionsCount>
@@ -300,10 +312,15 @@ __device__ Pair AdditionsReducer<maxExpressionsCount, maxVariablesCount, maxExpr
 }
 
 template <size_t maxExpressionsCount, size_t maxVariablesCount, size_t maxExpressionLength, size_t maxSubexpressionsCount>
-__device__ __host__ void AdditionsReducer<maxExpressionsCount, maxVariablesCount, maxExpressionLength, maxSubexpressionsCount>::replaceExpression(int index, int i, int j, int varIndex) {
-    int last = --expressionSizes[index];
-    expressions[index][i] = varIndex;
-    expressions[index][j] = expressions[index][last];
+__device__ __host__ void AdditionsReducer<maxExpressionsCount, maxVariablesCount, maxExpressionLength, maxSubexpressionsCount>::replaceExpression(int *expression, int &size, int index1, int index2, int varIndex) {
+    int j = index1;
+
+    for (int i = index1 + 1; i < size; i++)
+        if (i != index2)
+            expression[j++] = expression[i];
+
+    size--;
+    expression[j] = varIndex;
 }
 
 template <size_t maxExpressionsCount, size_t maxVariablesCount, size_t maxExpressionLength, size_t maxSubexpressionsCount>
